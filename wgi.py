f35cb9eff44dbec7f750988cc507c4dd83dfd45e
Sussyfreelandx <sussy@proctektexas.org>
2026-01-18 02:16:55 -0800

    Update wgi.py

--- wgi.py
+++ wgi.py
@@ -1,10 +1,4 @@
-from app import create_app, db
-from app.models import User, Campaign, Recipient
+from app import create_app
 
+# WSGI entry point for production servers like Gunicorn
 app = create_app()
-celery = app.extensions["celery"]
-
-@app.shell_context_processor
-def make_shell_context():
-    """Provides a default context for the `flask shell` command."""
-    return {'db': db, 'User': User, 'Campaign': Campaign, 'Recipient': Recipient}
