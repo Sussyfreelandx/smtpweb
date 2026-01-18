ed45a62e78c1c33519ca65d3ef9239f4e5c5dba4
Sussyfreelandx <sussy@proctektexas.org>
2026-01-18 05:11:14 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,4 @@
-# This is the entry point for the Gunicorn web server and Flask commands.
-import os
-from app import create_app
+# This is the entry point for the Gunicorn web server.
+from app import create_app, celery
 
-# The create_app function is the application factory.
-# We create the app instance here so it can be imported by Gunicorn and Flask.
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-# This part is not strictly necessary for deployment but is good practice
-# for running the app locally with `flask run`.
-if __name__ == '__main__':
-    app.run()
+app = create_app()
