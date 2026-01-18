f3629b5688fdd5495feb8e23ee9f02222824ae98
Sussyfreelandx <sussy@proctektexas.org>
2026-01-18 02:58:54 -0800

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
-bp = Blueprint('main', __name__)
+bp = Blueprint('main', __name__, template_folder='templates')
 
+# Import routes at the end to avoid circular dependencies
 from app.main import routes
