8d5e1a8840f9fa7ae72eae3acb332ea7752006ad
Sussyfreelandx <sussy@proctektexas.org>
2026-01-18 05:20:49 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,16 @@
-# This is the entry point for the Gunicorn web server.
-from app import create_app, celery
+from app import create_app, db
+from app.models import User, Campaign, Recipient, SMTPServer, Suppression
 
+# The application factory creates the app instance
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    """
+    Makes a dictionary of items available in the 'flask shell' context.
+    This is useful for debugging.
+    """
+    return {
+        'db': db, 'User': User, 'Campaign': Campaign, 'Recipient': Recipient,
+        'SMTPServer': SMTPServer, 'Suppression': Suppression
+    }
