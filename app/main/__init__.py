4862fe59a1498037609f58a6e5118b46a90afe7a
Sussyfreelandx <sussy@proctektexas.org>
2026-01-18 10:04:10 -0800

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,9 +1,1 @@
-from flask import Blueprint
 
-# Create the blueprint object for the 'main' part of your app.
-bp = Blueprint('main', __name__)
-
-# DO NOT IMPORT ROUTES HERE. This is the crucial change.
-# We will import the routes at the bottom of this file.
-
-from app.main import routes
