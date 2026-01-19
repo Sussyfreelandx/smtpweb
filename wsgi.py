e52e314da2de53c97521fd67829fc152dd2d9a8e
Sussyfreelandx <sussy@proctektexas.org>
2026-01-18 21:21:26 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,4 @@
-from app import create_app, db
-from app.models import User, Campaign, Recipient, SMTPServer, Suppression
+from app import create_app
 
 # The application factory creates the app instance
 app = create_app()
-
-@app.shell_context_processor
-def make_shell_context():
-    """
-    Makes a dictionary of items available in the 'flask shell' context.
-    This is useful for debugging.
-    """
-    return {
-        'db': db, 'User': User, 'Campaign': Campaign, 'Recipient': Recipient,
-        'SMTPServer': SMTPServer, 'Suppression': Suppression
-    }
