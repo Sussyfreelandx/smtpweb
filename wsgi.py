a69c37009e1a47d387b03088c3d360fb8f1f1dc3
Sussyfreelandx <sussy@proctektexas.org>
2026-01-19 01:01:32 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,23 @@
 from app import create_app, db
-from app.models import User, Campaign, Recipient, SMTPServer, Suppression
+from app.models import User, Campaign, Recipient, SMTPServer, Suppression, GlobalSettings
 
+# Create the application instance
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
+    """Makes objects available in flask shell."""
     return {
         'db': db,
         'User': User,
         'Campaign': Campaign,
         'Recipient': Recipient,
         'SMTPServer': SMTPServer,
-        'Suppression': Suppression
+        'Suppression': Suppression,
+        'GlobalSettings': GlobalSettings
     }
+
+
+if __name__ == '__main__':
+    app.run()
