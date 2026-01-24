2e84360d90805c429a9b54b3be171fc0ec11446f
Sussyfreelandx <sussy@proctektexas.org>
2026-01-23 22:23:01 -0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,6 @@
 # exit on error
 set -o errexit
 
-# Apply eventlet monkey-patching via a startup hook.
-# This is the most robust way to ensure Gunicorn's arbiter and workers
-# are all patched correctly, preventing the "do not call blocking functions" crash.
-exec gunicorn wsgi:app --worker-class eventlet --preload
+# Apply eventlet monkey-patching via a startup hook is done in wsgi.py
+# We simply run gunicorn using the config file we updated.
+exec gunicorn -c gunicorn.conf.py wsgi:app
